38d34a6428b93f62571093ed88bebfe6e39c4582
Robbie Jaeger <jaeger.rob@gmail.com>
2016-09-12 10:36:44 -0600

    Add link model and connect to user

---
+++ db/migrate/20160912163003_create_links.rb
@@ -0,0 +1,13 @@
+class CreateLinks < ActiveRecord::Migration[5.0]
+  def change
+    create_table :links do |t|
+      t.string :title
+      t.string :url
+      t.boolean :read, :default => false
+
+      t.timestamps
+    end
+
+    add_reference :links, :user, index: true
+  end
+end
