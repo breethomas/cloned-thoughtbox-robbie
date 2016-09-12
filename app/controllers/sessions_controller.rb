d9c91c3af41a0745e65e91e7d87c89e40fce2dfb
Robbie Jaeger <jaeger.rob@gmail.com>
2016-09-12 09:58:33 -0600

    User can login and validations work

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,23 @@
+class SessionsController < ApplicationController
+
+  def new
+  end
+
+  def create
+    user = User.find_by(email: params[:session][:email])
+    if user && user.authenticate(params[:session][:password])
+      session[:user_id] = user.id
+      flash[:success] = "Welcome!"
+      redirect_to links_path
+    else
+      flash.now[:danger] = "Invalid username or password. Try Again."
+      render :new
+    end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    flash[:success] = "Goodbye!"
+    redirect_to root_path
+  end
+end
