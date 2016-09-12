41d5eae0b65430e1349b5c497f32701d78f5a8da
Robbie Jaeger <jaeger.rob@gmail.com>
2016-09-12 11:06:57 -0600

    Added url validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
   has_many :links
 
+  validates :email, presence: true
+  validates :password, presence: true
   validates :email, uniqueness: true
   has_secure_password
 end
