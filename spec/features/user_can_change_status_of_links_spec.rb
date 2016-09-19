37e1b9b68f74efb54d1cca7376e0555027e33659
Robbie Jaeger <jaeger.rob@gmail.com>
2016-09-19 17:44:32 -0600

    Change test to mock user

--- spec/features/user_can_change_status_of_links_spec.rb
+++ spec/features/user_can_change_status_of_links_spec.rb
@@ -2,20 +2,11 @@ require 'rails_helper'
 
 RSpec.feature "User change read status of links" do
   scenario "mark a link as read", :js => true do
-    user = User.create(email: "Robbie", password: "password")
-    user.links.create(title: "New Link", url: "https://www.google.com")
+    user = User.create(email: "Robbie1", password: "password")
+    user.links.create(title: "NEW", url: "https://www.google.com")
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
 
-    visit login_path
-
-    fill_in "Email", with: "Robbie"
-    fill_in "Password", with: "password"
-    click_button("Login")
-
-    expect(page).to have_content "Logout"
-    expect(page).to have_content "Hello!"
-
-    expect(page).to have_content "New Link"
-    expect(page).to have_content "https://www.google.com"
+    visit links_path
 
     click_button("Mark as Read")
 
