bd3f105464c6e26536440804e61ab568d2b54648
Robbie Jaeger <jaeger.rob@gmail.com>
2016-09-20 14:00:51 -0600

    Make alphabetical sort test more thorough

--- spec/features/user_can_sort_links_alphabetically_spec.rb
+++ spec/features/user_can_sort_links_alphabetically_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
-RSpec.feature "User change read status of links" do
-  scenario "mark a link as read", :js => true do
+RSpec.feature "User sort alphabetically" do
+  scenario "A should be before B", :js => true do
     user = User.create(email: "Robbie1", password: "password")
     user.links.create(title: "B", url: "https://www.google.com")
     user.links.create(title: "A", url: "https://www.google.com")
@@ -9,11 +9,16 @@ RSpec.feature "User change read status of links" do
 
     visit links_path
 
-    click_button("Sort Link Titles Alphabetically")
-
     links = page.all(".link-container")
 
-    expect(links[0].find(".title").text).to have_content "A"
-    expect(links[1].find(".title").text).to have_content "B"
+    expect(links[0].find(".title").text).to have_content "B"
+    expect(links[1].find(".title").text).to have_content "A"
+
+    click_button("Sort Link Titles Alphabetically")
+
+    sortedLinks = page.all(".link-container")
+
+    expect(sortedLinks[0].find(".title").text).to have_content "A"
+    expect(sortedLinks[1].find(".title").text).to have_content "B"
   end
 end
