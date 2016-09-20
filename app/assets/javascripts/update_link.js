f5e160c9a13055f38de94ee8809bd9753697b7ea
Robbie Jaeger <jaeger.rob@gmail.com>
2016-09-20 10:08:32 -0600

    Move link attributes for read status update to function

--- app/assets/javascripts/update_link.js
+++ app/assets/javascripts/update_link.js
@@ -1,43 +1,46 @@
 $(document).ready(function() {
   $(".all-links").on("click", ".mark-read", function(){
-    var $button = $(this);
-    var $linkDiv = $button.parents(".link-container");
-    var linkID = $linkDiv.data("id");
-    var linkTitle = $linkDiv.find(".title").html();
-    var linkUrl = $linkDiv.find(".url").html();
+    linkAttributes = getLinkAttributes($(this));
 
     $.ajax({
-      url: '/links/' + linkID,
+      url: '/links/' + linkAttributes.id,
       method: "PATCH",
       dataType: "JSON",
-      data: {link: {title: linkTitle, url: linkUrl, read: "true"}}
+      data: {link: {title: linkAttributes.title, url: linkAttributes.url, read: "true"}}
     }).done( function() {
-      $button.removeClass("mark-read");
-      $button.addClass("mark-unread");
-      $button.html("Mark as Unread");
-      $button.parents(".panel-body").removeClass("read-false");
-      $button.parents(".panel-body").addClass("read-true");
+      linkAttributes.button.removeClass("mark-read");
+      linkAttributes.button.addClass("mark-unread");
+      linkAttributes.button.html("Mark as Unread");
+      linkAttributes.button.parents(".panel-body").removeClass("read-false");
+      linkAttributes.button.parents(".panel-body").addClass("read-true");
     });
   });
 
   $(".all-links").on("click", ".mark-unread", function(){
-    var $button = $(this);
-    var $linkDiv = $button.parents(".link-container");
-    var linkID = $linkDiv.data("id");
-    var linkTitle = $linkDiv.find(".title").html();
-    var linkUrl = $linkDiv.find(".url").html();
+    linkAttributes = getLinkAttributes($(this));
 
     $.ajax({
-      url: '/links/' + linkID,
+      url: '/links/' + linkAttributes.id,
       method: "PATCH",
       dataType: "JSON",
-      data: {link: {title: linkTitle, url: linkUrl, read: "false"}}
+      data: {link: {title: linkAttributes.title, url: linkAttributes.url, read: "false"}}
     }).done( function(){
-      $button.removeClass("mark-unread");
-      $button.addClass("mark-read");
-      $button.html("Mark as Read");
-      $button.parents(".panel-body").removeClass("read-true");
-      $button.parents(".panel-body").addClass("read-false");
+      linkAttributes.button.removeClass("mark-unread");
+      linkAttributes.button.addClass("mark-read");
+      linkAttributes.button.html("Mark as Read");
+      linkAttributes.button.parents(".panel-body").removeClass("read-true");
+      linkAttributes.button.parents(".panel-body").addClass("read-false");
     });
   });
 });
+
+function getLinkAttributes(button) {
+  var linkDiv = button.parents(".link-container");
+
+  return {
+    "button": button,
+    "id": linkDiv.data("id"),
+    "title": linkDiv.find(".title").html(),
+    "url": linkDiv.find(".url").html(),
+  }
+};
